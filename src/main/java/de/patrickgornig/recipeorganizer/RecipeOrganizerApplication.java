f859573312b42c072de0ebe3d1722f7f73198373
Patrick Gornig <patrickgornig@web.de>
2021-12-25 13:44:27 +0100

    first commit

---
+++ src/main/java/de/patrickgornig/recipeorganizer/RecipeOrganizerApplication.java
@@ -0,0 +1,13 @@
+package de.patrickgornig.recipeorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeOrganizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/patrickgornig/recipeorganizer/RecipeOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.patrickgornig.recipeorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeOrganizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
